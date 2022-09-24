712a90961ef661c1ef86e3af29089bb54ab56dd7
Bruno Carnavalli <carnavallibruno@gmail.com>
2022-09-24 15:52:14 -0300

    Fixed minor issue with the Keep Browsing button.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,6 @@ interface ButtonProps {
 
 export default function Button({ destination, buttonTitle }: ButtonProps) {
   const navigate = useNavigate()
-  const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
   return (
     <button onClick={() => destination.includes('https://') ? window.open('https://compass.uol/pt/home', '_blank') : navigate(`${destination}`) }>{buttonTitle}</button>
   )
